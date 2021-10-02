5867a60fb3c0ae068d2f5abcfc147ea9aa6f5396
jake <roarba011@gmail.com>
2021-10-01 18:17:00 -0600

    First commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+This code will only work on Linux and Mac OS X systems,
+and only in terminal emulators with true color available,
+
+How to check if your terminal has true color, write down this 
+command in your terminal:
+    
+printf "\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n"
+
+If the terminal does NOT print "TRUECOLOR" in red, your terminal 
+does not support it. 
+
+What terminal emulator support true color? 
+    Check this gist:
+        https://gist.github.com/XVilka/8346728#terminals--true-color
+"""
+from theming.themify.theme import Theme
---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'theming',
+  packages = ['theming'],
+  version = '0.1',
+  license='MIT',
+  description = 'A package to theme terminal scripts with custom colors and text formatting',
+  author = 'Rodrigo',
+  author_email = 'roarba011@gmail.com',
+  url = 'https://github.com/mrjakesir/theming',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['COLORS', 'SCRIPTING', 'THEME', 'THEMING', 'USELESS'],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
